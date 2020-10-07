e529ce987dcbe634d33d18e53cfdf194b0f3793e
git-austinmccollom <austinmccollom@gmail.com>
2020-10-07 15:35:53 -0500

    second query

--- queries.sql
+++ queries.sql
@@ -8,7 +8,10 @@ inner join Category on Product.CategoryId=Category.Id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
-
+SELECT Shipper.CompanyName, [Order].Id, [Order].ShippedDate
+FROM Shipper
+JOIN [Order] ON [Order].ShipVia = Shipper.Id
+WHERE [Order].ShippedDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
