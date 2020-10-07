95404357ae0b7d555ca11e581716070433a5b66b
git-austinmccollom <austinmccollom@gmail.com>
2020-10-07 17:00:01 -0500

    findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,19 @@
 const db = require('./connection.js')
 
 module.exports = {
-    find
+    find,
+    findById,
+    findSteps
 }
 
 function find() {
     return db('schemes');
+}
+
+function findById(id) {
+    return db('schemes').where({ id }).first()
+}
+
+function findSteps(id) {
+    return db('steps').where({ scheme_id: id })
 }
