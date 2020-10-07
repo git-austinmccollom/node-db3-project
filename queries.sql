4c88cb8fb9aacd36efd542ed4d1a44985ad323eb
git-austinmccollom <austinmccollom@gmail.com>
2020-10-07 16:07:55 -0500

    Fourth query

--- queries.sql
+++ queries.sql
@@ -6,12 +6,12 @@ select Product.ProductName, Category.CategoryName
 from Product
 inner join Category on Product.CategoryId=Category.Id
 
--- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 329 records.
+-- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 SELECT Shipper.CompanyName, [Order].Id, [Order].ShippedDate
 FROM Shipper
 JOIN [Order] ON [Order].ShipVia = Shipper.Id
-WHERE [Order].ShippedDate < '2012-08-09'
+WHERE [Order].OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
@@ -20,5 +20,9 @@ FROM OrderDetail
 JOIN Product ON Product.Id = OrderDetail.ProductId
 WHERE OrderDetail.OrderId = 10251
 
-
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT [Order].Id, Customer.CompanyName, Employee.LastName
+FROM [Order]
+JOIN Customer on [Order].CustomerId = Customer.Id
+JOIN Employee on [Order].EmployeeId = Employee.Id
